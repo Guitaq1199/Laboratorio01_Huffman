2d4317aef18f7ab126acb89d430e5e8104ea362a
unknown <mgrt99@gmail.com>
2019-09-26 21:33:13 -0600

    Agregado el metodo de compresión y escritura de archivo

--- Models/ClaseLZW.cs
+++ Models/ClaseLZW.cs
@@ -59,8 +59,6 @@ namespace Laboratorio1_MarceloRosales_CristianAzurdia_Huffman.Models
                     }
                 }
             }
-           
-            dicContadorDeCaracteresOriginales = dicContadorDeCaracteresOriginales.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
         }
         void ArmarDiccionarios(char caracterAVerificar)
         {
@@ -76,6 +74,121 @@ namespace Laboratorio1_MarceloRosales_CristianAzurdia_Huffman.Models
             }
 
 
+        }
+        public void compresion()
+        {
+            string AuxiliarDeconversion;
+            string temporalSigBuffer = "";
+            var buffer = new byte[bufferLenght];
+            using (var file = new FileStream("C:/Users/mgrt9/desktop/PHuff.txt", FileMode.Open))
+            {
+                using (var reader = new BinaryReader(file))
+                {
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+                    {
+                        buffer = reader.ReadBytes(bufferLenght);
+                        AuxiliarDeconversion = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+                        temporalSigBuffer = ArmarDiccionarioDeCaracteresComplementarios(temporalSigBuffer + AuxiliarDeconversion);
+                    }
+                }
+            }
+        }
+        public string ArmarDiccionarioDeCaracteresComplementarios(string Cadena)
+        {
+            string aux = "";
+            string temporalSigBuffer = "";
+            Queue<int> CombinacionBytes = new Queue<int>();
+            string valorDiccionario = "";
+            string CadenaTotal = "";
+
+            for (int i = 0; i < Cadena.Length; i++)
+            {
+                if (DiccionarioComplementarioDeCaracteres.ContainsKey(Cadena[i].ToString()))
+                {
+                    valorDiccionario = aux;
+                    aux += Cadena[i].ToString();
+                    if (i + 1 != Cadena.Length)
+                    {
+                        if (!DiccionarioComplementarioDeCaracteres.ContainsKey(aux))
+                        {
+
+                            DiccionarioComplementarioDeCaracteres.Add(aux, DiccionarioComplementarioDeCaracteres.Count() + 1);
+                            aux = Cadena[i].ToString();
+                            CadenaTotal += DiccionarioComplementarioDeCaracteres[valorDiccionario];
+
+                        }
+                    }
+                    else // cuando el indice +1 sea igual al lenght de la cadnea
+                    {
+
+                        if (!DiccionarioComplementarioDeCaracteres.ContainsKey(aux)) // verifica si no esta, entonces lo agrega
+                        {
+                            DiccionarioComplementarioDeCaracteres.Add(aux, DiccionarioComplementarioDeCaracteres.Count() + 1);
+                            CadenaTotal += DiccionarioComplementarioDeCaracteres[valorDiccionario];
+                        }
+                        else // si esta, etonces lo iguala al valor de retorno y retornará
+                        {
+                            temporalSigBuffer = aux;
+                        }
+                    }
+                }
+            }
+
+            Escritura(CadenaTotal);
+            return temporalSigBuffer;
+        }
+
+
+        public void EscribirDiccionario()
+        {
+            string rut = RutaDeDescarga + "/" + NombreDelArchivo + "_Comprimido.txt";
+            using (var file = new FileStream(rut, FileMode.Create))
+            {
+                using (var escritor = new StreamWriter(file))
+                {
+                    foreach (var item in dicContadorDeCaracteresOriginales)
+                    {
+                        escritor.Write(item.Key + "|" + item.Value + "|");
+                    }
+                    escritor.Write("┼");
+                }
+
+            }
+        }
+        void Escritura(string cadena)
+        {
+            var buffer = new byte[cadena.Length];
+            string aux = "";
+            string rut = RutaDeDescarga + "/" + NombreDelArchivo + "_Comprimido.txt";
+            using (var file = new FileStream(rut, FileMode.Append))
+            {
+                using (var escritor = new BinaryWriter(file))
+                {
+
+                    for (int i = 0; i < cadena.Length; i += 2)
+                    {
+                        if (i + 1 == cadena.Length)
+                        {
+                            aux = cadena[i].ToString();
+                        }
+                        else
+                        {
+                            aux = cadena[i].ToString() + cadena[i + 1].ToString();
+                        }
+
+
+                        escritor.Write(Convert.ToByte(aux));
+
+                    }
+
+                }
+            }
+        }
+        public void Limpiar()
+        {
+            DiccionarioComplementarioDeCaracteres.Clear();
+            dicContadorDeCaracteresOriginales.Clear();
         }
 
     }
